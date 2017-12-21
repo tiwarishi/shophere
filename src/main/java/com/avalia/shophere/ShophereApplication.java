a1eebea8c8aca62a45ee3d61e75d9bb0b6adaf88
tiwarishi <rajrishi0504@gmail.com>
2017-12-21 15:10:25 +0530

    Project initialization

---
+++ src/main/java/com/avalia/shophere/ShophereApplication.java
@@ -0,0 +1,12 @@
+package com.avalia.shophere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShophereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShophereApplication.class, args);
+	}
+}
